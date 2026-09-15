bd940342d77c036c3ba0a4403a91cdca8c488200
Lucas Oliveira <lucasleiteoliveira09@gmail.com>
2026-09-15 01:35:56 +0000

    Exercicio 3 FInalizado novamente!

--- data-binding/src/app/lista-exercicios-1/lista-exercicios-1-module.ts
+++ data-binding/src/app/lista-exercicios-1/lista-exercicios-1-module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { ListaExercicios1RoutingModule } from './lista-exercicios-1-routing-module';
 import { ApresentacaoUser } from './apresentacao-user/apresentacao-user';
 import { CalculandoComInterpolation } from './calculando-com-interpolation/calculando-com-interpolation';
+import { ImagemDinamica } from './imagem-dinamica/imagem-dinamica';
 
 @NgModule({
-  declarations: [ApresentacaoUser, CalculandoComInterpolation],
+  declarations: [ApresentacaoUser, CalculandoComInterpolation, ImagemDinamica],
   imports: [CommonModule, ListaExercicios1RoutingModule, FormsModule],
 })
 export class ListaExercicios1Module {}
--- data-binding/src/app/lista-exercicios-1/lista-exercicios-1-routing-module.ts
+++ data-binding/src/app/lista-exercicios-1/lista-exercicios-1-routing-module.ts
@@ -1,11 +1,14 @@
+import { ImagemDinamica } from './imagem-dinamica/imagem-dinamica';
 import { CalculandoComInterpolation } from './calculando-com-interpolation/calculando-com-interpolation';
 import { ApresentacaoUser } from './apresentacao-user/apresentacao-user';
+
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: 'apresentacao-user', component: ApresentacaoUser },
-  {path: 'calculando-com-interpolation', component: CalculandoComInterpolation }
+  {path: 'calculando-com-interpolation', component: CalculandoComInterpolation },
+  {path: 'imagem-dinamica', component: ImagemDinamica }
 ];
 
 
