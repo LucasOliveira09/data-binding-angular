eedc6b31260d0ec3da2ba5d2c053c480114e5555
Lucas Oliveira <lucasleiteoliveira09@gmail.com>
2026-09-23 01:33:49 +0000

    Lista 2 Exercicio 3

--- data-binding/src/app/lista2-exercicios/lista2-exercicios-module.ts
+++ data-binding/src/app/lista2-exercicios/lista2-exercicios-module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FormsModule } from '@angular/forms';
+
 import { Lista2ExerciciosRoutingModule } from './lista2-exercicios-routing-module';
 import { ExebicaoDeMensagem } from './exebicao-de-mensagem/exebicao-de-mensagem';
 import { SituacaoDoUsuario } from './situacao-do-usuario/situacao-do-usuario';
@@ -8,6 +10,6 @@ import { VerificacaoDeIdade } from './verificacao-de-idade/verificacao-de-idade'
 
 @NgModule({
   declarations: [ExebicaoDeMensagem, SituacaoDoUsuario, VerificacaoDeIdade],
-  imports: [CommonModule, Lista2ExerciciosRoutingModule],
+  imports: [CommonModule, Lista2ExerciciosRoutingModule, FormsModule],
 })
 export class Lista2ExerciciosModule {}
--- data-binding/src/app/lista2-exercicios/verificacao-de-idade/verificacao-de-idade.ts
+++ data-binding/src/app/lista2-exercicios/verificacao-de-idade/verificacao-de-idade.ts
@@ -6,4 +6,24 @@ import { Component } from '@angular/core';
   styleUrl: './verificacao-de-idade.css',
   templateUrl: './verificacao-de-idade.html',
 })
-export class VerificacaoDeIdade {}
+export class VerificacaoDeIdade {
+  idade : number = 0;
+  mensagem : string = '';
+
+  validaIdade(): void {
+    if(this.idade <= 0){
+      return
+    }
+
+    if (this.idade <= 12) {
+      this.mensagem = 'Criança';
+    } else if (this.idade > 12 && this.idade <= 17){
+      this.mensagem = 'Adolescente';
+    } else if (this.idade >= 60){
+      this.mensagem = 'Idoso'
+    } else if (this.idade > 17 && this.idade < 60) {
+      this.mensagem = 'Adulto'
+    }
+  }
+
+}
